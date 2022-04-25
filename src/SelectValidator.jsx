d4df25f1458baaf172c03a991c78c6b2667920dd
Aftabv1 <aftabrehan52422@gmail.com>
2022-04-26 04:55:05 +0500

    Add Simple Form UI with Source Code

---
+++ src/SelectValidator.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable */
+import React from "react";
+import TextField from "@mui/material/TextField";
+/* eslint-enable */
+import { ValidatorComponent } from "react-form-validator-core";
+
+export default class SelectValidator extends ValidatorComponent {
+  renderValidatorComponent() {
+    /* eslint-disable no-unused-vars */
+    const {
+      error,
+      errorMessages,
+      validators,
+      requiredError,
+      helperText,
+      validatorListener,
+      withRequiredValidator,
+      containerProps,
+      ...rest
+    } = this.props;
+    const { isValid } = this.state;
+    return (
+      <TextField
+        {...rest}
+        select
+        error={!isValid || error}
+        helperText={(!isValid && this.getErrorMessage()) || helperText}
+      />
+    );
+  }
+}
